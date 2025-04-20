f03bfa3c9e567a823976dbdd3fd6a0ce6bca4a05
Shristi Shrivastava <152075868+shri2310sti@users.noreply.github.com>
2025-04-20 19:02:56 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Find Only Repetitive Element from 1 to n-1/find-only-repetitive-element-from-1-to-n1.java
@@ -0,0 +1,46 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String args[]) throws IOException {
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        while (t-- > 0) {
+            // Read the array from input line
+            String inputLine = read.readLine().trim();
+            String[] inputArr = inputLine.split("\\s+");
+            int n = inputArr.length;
+            int arr[] = new int[n];
+            for (int i = 0; i < n; i++) {
+                arr[i] = Integer.parseInt(inputArr[i]);
+            }
+
+            Solution ob = new Solution();
+            System.out.println(ob.findDuplicate(arr));
+            System.out.println("~");
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+// User function Template for Java
+class Solution {
+    public int findDuplicate(int[] arr) {
+        // code here
+        if (arr == null || arr.length == 0) {
+            return -1;
+        }
+        int length = arr.length - 1;
+        long totalSum = ((long)length * ((long)(length + 1))) / 2;
+        long currentSum = 0;
+        for (int index = 0; index <= length; index++) {
+            currentSum += arr[index];
+        }
+        return (int)(currentSum - totalSum);
+    }
+}
