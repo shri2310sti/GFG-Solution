8818d711997f5823587e866a0e9fabb5471a12f2
Shristi Shrivastava <152075868+shri2310sti@users.noreply.github.com>
2025-05-13 17:10:35 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/nCr/n-cr.java
@@ -0,0 +1,43 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.lang.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+
+        while (t-- > 0) {
+            int n = sc.nextInt();
+            int r = sc.nextInt();
+            Solution ob = new Solution();
+            System.out.println(ob.nCr(n, r));
+            System.out.println("~");
+        }
+
+        sc.close();
+    }
+}
+// } Driver Code Ends
+
+
+class Solution {
+    public int nCr(int n, int r) {
+        if (r > n) return 0;
+        if (r == 0 || r == n) return 1;
+
+        int[][] dp = new int[n + 1][r + 1];
+
+        for (int i = 0; i <= n; i++) {
+            for (int j = 0; j <= Math.min(i, r); j++) {
+                if (j == 0 || j == i)
+                    dp[i][j] = 1;
+                else
+                    dp[i][j] = dp[i - 1][j - 1] + dp[i - 1][j];
+            }
+        }
+
+        return dp[n][r];
+    }
+}
