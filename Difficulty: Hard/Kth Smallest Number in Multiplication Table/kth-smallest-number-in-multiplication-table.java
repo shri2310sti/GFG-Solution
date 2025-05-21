3335723f9d4d5b04096c821d3a4b426edc9ffca6
Shristi Shrivastava <152075868+shri2310sti@users.noreply.github.com>
2025-05-21 08:20:59 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Hard/Kth Smallest Number in Multiplication Table/kth-smallest-number-in-multiplication-table.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int kthSmallest(int m, int n, int k) {
+        int low = 1, high = m * n;
+
+        while (low < high) {
+            int mid = low + (high - low) / 2;
+            if (countLessEqual(mid, m, n) < k) {
+                low = mid + 1;
+            } else {
+                high = mid;
+            }
+        }
+
+        return low;
+    }
+
+    private int countLessEqual(int x, int m, int n) {
+        int count = 0;
+        for (int i = 1; i <= m; i++) {
+            count += Math.min(x / i, n);
+        }
+        return count;
+    }
+}
