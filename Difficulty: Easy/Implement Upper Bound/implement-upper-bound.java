c4f61eea739c3ac32036ed91eb7b11cbfc428ce2
Shristi Shrivastava <152075868+shri2310sti@users.noreply.github.com>
2025-06-15 19:02:04 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Implement Upper Bound/implement-upper-bound.java
+++ Difficulty: Easy/Implement Upper Bound/implement-upper-bound.java
@@ -3,11 +3,14 @@ class Solution {
         // code here
         int low = 0, high = arr.length - 1;
         int ans = arr.length;
+        
         while(low <= high){
-            int mid = (high + low) / 2;
-            if(target < arr[mid]) {
-                 ans = mid;
-                high = mid - 1;
+            
+            int mid = (low + high) / 2;
+            
+            if(arr[mid] > target){
+                ans = mid;
+                high = mid-1;
             }
             else low = mid + 1;
         }
