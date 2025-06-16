024ecac51f8e4da31598b97edce12a3a13f499b6
Shristi Shrivastava <152075868+shri2310sti@users.noreply.github.com>
2025-06-16 11:33:55 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Floor in a Sorted Array/floor-in-a-sorted-array.java
+++ Difficulty: Easy/Floor in a Sorted Array/floor-in-a-sorted-array.java
@@ -4,12 +4,13 @@ class Solution {
         // write code here
         int low = 0, high = arr.length - 1;
         int ans = -1;
+        
         while(low <= high){
             int mid = (low + high) / 2;
+            
             if(arr[mid] <= x){
-                ans = mid ;
+                ans = mid;
                 low = mid + 1;
-                
                 
             } else high = mid - 1;
         }
