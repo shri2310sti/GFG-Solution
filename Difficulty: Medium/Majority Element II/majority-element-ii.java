9d31e904b15883b95e96b5b19e4dc321083ae87a
Shristi Shrivastava <152075868+shri2310sti@users.noreply.github.com>
2025-07-26 10:36:17 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Majority Element II/majority-element-ii.java
@@ -0,0 +1,54 @@
+
+
+
+class Solution {
+    public List<Integer> findMajority(int[] nums) {
+        List<Integer> result = new ArrayList<>();
+        if (nums == null || nums.length == 0) {
+            return result;
+        }
+
+        // Step 1: Candidate selection using Boyer-Moore Voting Algorithm
+        int candidate1 = 0, candidate2 = 0, count1 = 0, count2 = 0;
+        for (int num : nums) {
+            if (num == candidate1) {
+                count1++;
+            } else if (num == candidate2) {
+                count2++;
+            } else if (count1 == 0) {
+                candidate1 = num;
+                count1 = 1;
+            } else if (count2 == 0) {
+                candidate2 = num;
+                count2 = 1;
+            } else {
+                count1--;
+                count2--;
+            }
+        }
+
+        // Step 2: Verification of candidates
+        count1 = 0;
+        count2 = 0;
+        for (int num : nums) {
+            if (num == candidate1) {
+                count1++;
+            } else if (num == candidate2) {
+                count2++;
+            }
+        }
+
+        // Add to result if count > n/3
+        int n = nums.length;
+        if (count1 > n / 3) {
+            result.add(candidate1);
+        }
+        if (count2 > n / 3) {
+            result.add(candidate2);
+        }
+
+        // Step 3: Sort the result
+        Collections.sort(result);
+        return result;
+    }
+}
