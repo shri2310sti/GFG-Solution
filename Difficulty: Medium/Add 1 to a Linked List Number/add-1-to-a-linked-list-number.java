ba1837d1ea350333488d0f240039ddb0aa3d083b
Shristi Shrivastava <152075868+shri2310sti@users.noreply.github.com>
2025-08-06 12:41:39 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Add 1 to a Linked List Number/add-1-to-a-linked-list-number.java
+++ Difficulty: Medium/Add 1 to a Linked List Number/add-1-to-a-linked-list-number.java
@@ -13,37 +13,20 @@ class Node{
 class Solution {
     public Node addOne(Node head) {
         // code here.
-        head = reverse(head);
-        Node temp = head; int carry = 1;
-        while(temp != null){
-            temp.data = temp.data + carry;
-            if(temp.data < 10){
-                carry = 0;
-                break;
-            } else {
-                temp.data = 0;
-                carry = 1;
-            }
-            temp = temp.next;
-        }
+        int carry = helper(head);
         if(carry == 1){
-            Node newNode = new Node(1);
-            head = reverse(head);
-            newNode.next = head;
-            return newNode;
-        } 
-        head = reverse(head);
+            Node nw = new Node(1);
+            nw.next = head;
+            head = nw;
+        }
         return head;
     }
-    private static Node reverse(Node head){
-        Node temp = head;
-        Node prev = null;
-        while(temp != null){
-            Node t = temp.next;
-            temp.next = prev;
-            prev = temp;
-            temp = t;
-        }
-        return prev;
+    private static int helper(Node temp){
+        if(temp == null) return 1;
+        int carry = helper(temp.next);
+        temp.data += carry;
+        if(temp.data < 10) return 0;
+        temp.data = 0;
+        return 1;
     }
 }
