4532f5f90162f5601b5eeb56daf3d4ea4b423cb1
Shristi Shrivastava <152075868+shri2310sti@users.noreply.github.com>
2025-10-10 21:37:11 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Number of greater elements to the right/number-of-greater-elements-to-the-right.java
+++ Difficulty: Medium/Number of greater elements to the right/number-of-greater-elements-to-the-right.java
@@ -1,17 +1,28 @@
-
 class Solution {
-    public static int[] count_NGE(int arr[], int indices[]) {
-        // code here
-        int[] ans = new int[indices.length];
-        
-        for(int i = 0; i < indices.length; i++){
-            int count = 0;
+  public static int[] count_NGE(int arr[], int indices[]) {
+      int queries = indices.length;
+      int n = arr.length;
+    int[] res=new int[queries];
+    int[] ngeCnt=new int[n];
+    Stack<Integer> decStack=new Stack<>();
+    Stack<Integer> inStack=new Stack<>();
+    
+    for (int i=n-1;i>=0;i--) {
+        while (!inStack.isEmpty() && inStack.peek()>arr[i])
+            decStack.push(inStack.pop());
             
-            for(int j = indices[i] + 1; j<arr.length; j++){
-                if(arr[indices[i]] < arr[j] ) count++;
-            }
-            ans[i] = count;
-        }
-        return ans;
+        while (!decStack.isEmpty() && decStack.peek()<=arr[i])
+            inStack.push(decStack.pop());
+            
+        if (!decStack.isEmpty())
+            ngeCnt[i]=decStack.size();
+            
+        decStack.push(arr[i]);
     }
+    
+    for (int q=0;q<queries;q++)
+        res[q]=ngeCnt[indices[q]];
+        
+    return res;
+  }
 }
