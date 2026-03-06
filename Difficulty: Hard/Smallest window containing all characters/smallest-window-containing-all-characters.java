27f64c2996d089986256255e9c5a318b05e66431
Shristi Shrivastava <152075868+shri2310sti@users.noreply.github.com>
2026-03-06 07:55:04 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Hard/Smallest window containing all characters/smallest-window-containing-all-characters.java
+++ Difficulty: Hard/Smallest window containing all characters/smallest-window-containing-all-characters.java
@@ -1,75 +1,39 @@
 class Solution {
-    // Function to find the smallest window in the string s consisting
-    // of all the characters of string p.
-    public static String smallestWindow(String s1, String s2) {
-        
-        // a variant of the problem smallest substring containing distinct character of the string itself
-        
-        StringBuilder ans=new StringBuilder();
-        
-        int n=s1.length();
-        int m=s2.length();
-        
-        if(s2.length()>s1.length()){
-            return ans.toString();
-        }
-        
-        HashMap<Character,Integer> window=new HashMap<>();
-        HashMap<Character,Integer> hms2=new HashMap<>();
-        
-        for(char c : s2.toCharArray()){
-            hms2.put(c , hms2.getOrDefault(c,0)+1);
-        }
-        
-        HashMap<Character,Integer> required=new HashMap<>(hms2);
+    public static String minWindow(String s, String p) {
+        if (s == null || p == null) return "";
+        int n = s.length(), m = p.length();
+        if (m > n) return "";
 
-         //window  include all character distinct
-        for(char c : required.keySet()){
-             window.put(c,0);
-        }
-        
-        int start=-1;
-        int min=Integer.MAX_VALUE;
-        int i=0;//left 
-        int j=0;//right
-        int count=0; //required character
-        
-        while(j<n){
+        int[] need = new int[128];
+        for (int i = 0; i < m; i++) need[p.charAt(i)]++;
+
+        int have = 0;              
+        int left = 0;
+        int minLen = Integer.MAX_VALUE;
+        int minLeft = 0;
+
+        for (int right = 0; right < n; right++) {
+            char rc = s.charAt(right);
+       
+            need[rc]--;
             
-            char c=s1.charAt(j);
-            
-            if(required.containsKey(c)){
-                window.put(c,window.get(c)+1);
-                ///mindful of duplicates
-                if(window.get(c)<=required.get(c)){
-                    count++;
+            if (need[rc] >= 0) have++;
+
+            while (have == m) {
+                int windowLen = right - left + 1;
+                if (windowLen < minLen) {
+                    minLen = windowLen;
+                    minLeft = left;
                 }
+
+                char lc = s.charAt(left++);
+             
+                need[lc]++;
+               
+                if (need[lc] > 0) have--;
             }
-            
-            while(count==m){
-                
-                if(j-i+1<min){
-                    min=j-i+1;
-                    start=i;
-                }
-                //shrinking from left
-                char ch=s1.charAt(i);
-                if(required.containsKey(ch)){
-                    window.put(ch,window.get(ch)-1);
-                    if(window.get(ch)<required.get(ch)){
-                        count--;
-                    }
-                }
-                i++;
-            }
-            j++;
         }
-        if (start==-1){
-            return "";
-        }
-        return s1.substring(start,start+min);
-        
-        
-        
+
+        return minLen == Integer.MAX_VALUE ? "" : s.substring(minLeft, minLeft + minLen);
     }
 }
