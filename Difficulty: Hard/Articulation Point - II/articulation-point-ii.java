d92ca07a69e43149a5221d607a4d4edd3f582805
Shristi Shrivastava <152075868+shri2310sti@users.noreply.github.com>
2026-03-28 07:32:44 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Hard/Articulation Point - II/articulation-point-ii.java
+++ Difficulty: Hard/Articulation Point - II/articulation-point-ii.java
@@ -1,97 +1,74 @@
-//{ Driver Code Starts
 import java.util.*;
-
-public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int tc = sc.nextInt();
-        while (tc-- > 0) {
-            int V = sc.nextInt();
-            int E = sc.nextInt();
-            int[][] edges = new int[E][2];
-            for (int i = 0; i < E; i++) {
-                edges[i][0] = sc.nextInt();
-                edges[i][1] = sc.nextInt();
-            }
-
-            Solution obj = new Solution();
-            ArrayList<Integer> ans = obj.articulationPoints(V, edges);
-            Collections.sort(ans);
-            for (int val : ans) {
-                System.out.print(val + " ");
-            }
-            System.out.println();
-            System.out.println("~");
-        }
-    }
-}
-// } Driver Code Ends
-
 
 class Solution {
     static ArrayList<Integer> articulationPoints(int V, int[][] edges) {
-        ArrayList<Integer>[] graph = new ArrayList[V];
-        for (int i = 0; i < V; i++) graph[i] = new ArrayList<>();
+        ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
+        for (int i = 0; i < V; i++) adj.add(new ArrayList<>());
 
-        for (int[] edge : edges) {
-            int u = edge[0], v = edge[1];
-            graph[u].add(v);
-            graph[v].add(u);
+        for (int[] e : edges) {
+            int u = e[0], v = e[1];
+            adj.get(u).add(v);
+            adj.get(v).add(u);
         }
 
         int[] disc = new int[V];
         int[] low = new int[V];
         int[] parent = new int[V];
-        boolean[] visited = new boolean[V];
-        boolean[] ap = new boolean[V];  // articulation point
-        Arrays.fill(disc, -1);
-        Arrays.fill(low, -1);
+        int[] childCount = new int[V];
+        int[] itIndex = new int[V];
+        boolean[] ap = new boolean[V];
+
         Arrays.fill(parent, -1);
 
-        int time = 0;
-        for (int i = 0; i < V; i++) {
-            if (!visited[i]) {
-                dfs(i, time, disc, low, parent, visited, ap, graph);
+        int time = 1;
+        Deque<Integer> stack = new ArrayDeque<>();
+
+        for (int start = 0; start < V; start++) {
+            if (disc[start] != 0) continue;
+
+            stack.push(start);
+
+            while (!stack.isEmpty()) {
+                int u = stack.peek();
+
+                if (disc[u] == 0) {
+                    disc[u] = low[u] = time++;
+                }
+
+                if (itIndex[u] < adj.get(u).size()) {
+                    int v = adj.get(u).get(itIndex[u]++);
+
+                    if (v == parent[u]) continue;
+
+                    if (disc[v] == 0) {
+                        parent[v] = u;
+                        childCount[u]++;
+                        stack.push(v);
+                    } else {
+                        low[u] = Math.min(low[u], disc[v]);
+                    }
+                } else {
+                    stack.pop();
+
+                    int p = parent[u];
+                    if (p == -1) {
+                        if (childCount[u] > 1) ap[u] = true;
+                    } else {
+                        low[p] = Math.min(low[p], low[u]);
+                        if (parent[p] != -1 && low[u] >= disc[p]) {
+                            ap[p] = true;
+                        }
+                    }
+                }
             }
         }
 
-        ArrayList<Integer> res = new ArrayList<>();
+        ArrayList<Integer> ans = new ArrayList<>();
         for (int i = 0; i < V; i++) {
-            if (ap[i]) res.add(i);
+            if (ap[i]) ans.add(i);
         }
 
-        if (res.isEmpty()) res.add(-1);
-        return res;
-    }
-
-    private static void dfs(int u, int time, int[] disc, int[] low, int[] parent,
-                            boolean[] visited, boolean[] ap, ArrayList<Integer>[] graph) {
-        visited[u] = true;
-        disc[u] = low[u] = ++time;
-        int children = 0;
-
-        for (int v : graph[u]) {
-            if (!visited[v]) {
-                children++;
-                parent[v] = u;
-                dfs(v, time, disc, low, parent, visited, ap, graph);
-
-                // update low value after visiting child
-                low[u] = Math.min(low[u], low[v]);
-
-                // Rule 1: root of DFS tree with 2+ children
-                if (parent[u] == -1 && children > 1) {
-                    ap[u] = true;
-                }
-
-                // Rule 2: not root, and low[v] >= disc[u]
-                if (parent[u] != -1 && low[v] >= disc[u]) {
-                    ap[u] = true;
-                }
-            } else if (v != parent[u]) {
-                // back edge
-                low[u] = Math.min(low[u], disc[v]);
-            }
-        }
+        if (ans.isEmpty()) ans.add(-1);
+        return ans;
     }
 }
